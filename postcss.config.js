2941f1079dabaf639d25a0248638ed76f9d2aca8
a-void <avitiuk@me.com>
2018-07-06 14:55:07 -0400

    Update: postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,11 @@ module.exports = {
   plugins: {
     'postcss-import': { path: ['src'] },
     'postcss-preset-env': {
-      browsers: ['last 2 versions', '> 1%', 'opera 12.1', 'bb 10', 'android 4']
+      browsers: [
+        'last 2 versions',
+        'ie >= 10',
+        'Safari >= 8'
+      ]
     }
   }
 }
