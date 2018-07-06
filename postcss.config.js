a52735ca2de7b20f8b5b89be424401e42b65b14e
a-void <avitiuk@me.com>
2018-07-06 11:37:50 -0400

    Update: dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: {
     'postcss-import': { path: ['src'] },
-    'postcss-cssnext': {
+    'postcss-preset-env': {
       browsers: ['last 2 versions', '> 1%', 'opera 12.1', 'bb 10', 'android 4']
     }
   }
